e2ba9a7ee378da94f5ffa20adf19279747493b08
Dongmin <pdm001125@naver.com>
2024-05-13 15:26:09 +0900

    feat/#5: chipvisible Setting

--- app/src/main/java/com/nowsopt/spotify/presentation/main/HomeFragment.kt
+++ app/src/main/java/com/nowsopt/spotify/presentation/main/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.nowsopt.spotify.presentation.main
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.nowsopt.spotify.databinding.FragmentHomeBinding
 import com.nowsopt.spotify.util.base.BindingFragment
 
@@ -11,4 +14,19 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>() {
         container: ViewGroup?,
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chipCh.setOnClickListener {
+            binding.chipX.isVisible = true
+            binding.chipPodcast.isVisible = false
+            binding.chipHeart.isVisible = false
+        }
+        binding.chipX.setOnClickListener {
+            binding.chipX.isVisible = false
+            binding.chipCh.isVisible = true
+            binding.chipPodcast.isVisible = true
+            binding.chipHeart.isVisible = true
+        }
+    }
 }
---
+++ app/src/main/java/com/nowsopt/spotify/presentation/main/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.nowsopt.spotify.presentation.main
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+}
