8a693f08af21b34d26dc25d5724d37380765e007
YuJeongHyun <0703olivia@naver.com>
2024-05-19 21:59:53 +0900

    feat/#11: artist 화면 구성

---
+++ app/src/main/java/com/nowsopt/spotify/presentation/main/artist/ArtistActivity.kt
@@ -0,0 +1,19 @@
+package com.nowsopt.spotify.presentation.main.artist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nowsopt.spotify.R
+import com.nowsopt.spotify.databinding.ActivityArtistBinding
+import com.nowsopt.spotify.databinding.ActivityMainBinding
+import com.nowsopt.spotify.util.base.BindingActivity
+
+class ArtistActivity : BindingActivity<ActivityArtistBinding>(ActivityArtistBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
