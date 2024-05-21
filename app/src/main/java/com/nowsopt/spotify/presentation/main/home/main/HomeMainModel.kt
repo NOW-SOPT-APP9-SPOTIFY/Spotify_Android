3e2738c6bd2406ea05606e9da2238e46ad4388e4
SYAAINN <alswo08319@gmail.com>
2024-05-21 22:15:42 +0900

    feat/#19: HomeMainModel 과 Mock 데이터 생성

--- app/src/main/java/com/nowsopt/spotify/presentation/main/home/main/HomeMainModel.kt
+++ app/src/main/java/com/nowsopt/spotify/presentation/main/home/main/HomeMainModel.kt
@@ -1,5 +1,37 @@
 package com.nowsopt.spotify.presentation.main.home.main
 
-class HomeMainModel {
+sealed class HomeMainModel {
+    data class RecommendMix(
+        val imageUrl: String,
+        val theme: String,
+        val artists: String
+    ): HomeMainModel()
 
+    data class TodayHitSong(
+        val imageUrl: String,
+        val title: String,
+        val artist: String
+    ): HomeMainModel()
+
+    data class PopularArtist(
+        val imageUrl: String,
+        val artist: String
+    ): HomeMainModel()
+
+    data class RecentPlay(
+        val imageUrl: String,
+        val title: String
+    ): HomeMainModel()
+
+    data class ListenableShow(
+        val imageUrl: String,
+        val genre: String,
+        val title: String,
+        val artist: String
+    ): HomeMainModel()
+
+    data class PopularRadio(
+        val imageUrl: String,
+        val artists: String
+    ): HomeMainModel()
 }
